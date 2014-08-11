27be988271ed243675c8eaae195175a5ac758e0d
Mark Schmidt <mark.schmidt@xing.com>
2014-08-11 20:21:21 +0200

    minor refactorings in DataFetcher

--- lib/whats_next/data_fetcher.ex
+++ lib/whats_next/data_fetcher.ex
@@ -5,25 +5,19 @@ defmodule WhatsNext.DataFetcher do
   def fetch(series) do
     series
     |> fetch_series_id
-    |> decode_response
+    |> decode_json
     |> extract_series_url
-    |> fetch_episode_data
+    |> fetch_data
   end
 
   defp fetch_series_id(series) do
     query = "allintitle: site:epguides.com " <> series
     url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=" <> URI.encode(query)
-    case HTTPotion.get(url) do
-      %Response{body: body, status_code: status, headers: _headers }
-      when status in 200..299 ->
-        { :ok, body }
-      %Response{body: body, status_code: _status, headers: _headers } ->
-        { :error, body }
-    end
+    fetch_data(url)
   end
 
-  defp decode_response({:ok, body}), do: JSON.decode(body)
-  defp decode_response({:error, msg}) do
+  defp decode_json({:ok, body}), do: JSON.decode(body)
+  defp decode_json({:error, msg}) do
     error = JSON.decode(msg)["message"]
     IO.puts "Error fetching from series name: #{error}"
     System.halt(2)
@@ -33,13 +27,12 @@ defmodule WhatsNext.DataFetcher do
     (response["responseData"]["results"] |> hd)["unescapedUrl"]
   end
 
-  defp fetch_episode_data(series_url) do
-    case HTTPotion.get(series_url) do
+  defp fetch_data(url) do
+    case HTTPotion.get(url) do
       %Response{body: body, status_code: status, headers: _headers }
       when status in 200..299 ->
         { :ok, body }
       %Response{body: body, status_code: _status, headers: headers } ->
-        IO.puts headers[:location]
         { :error, body }
     end
   end
