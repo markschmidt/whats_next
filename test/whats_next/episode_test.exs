b127dbb89c923d161b04649d6d071b7a9489bc33
Mark Schmidt <mark.schmidt@xing.com>
2014-08-18 21:17:34 +0200

    test refactorings

--- test/whats_next/episode_test.exs
+++ test/whats_next/episode_test.exs
@@ -3,25 +3,23 @@ defmodule WhatsNext.EpisodeTest do
 
   import WhatsNext.Episode
 
+  @example_list [["1x01", "2011-06-23"], ["1x02", "2011-06-30"], ["1x03", "2011-07-07"]]
+
   test "#next_air_date should return next entry in list" do
-    assert "2011-06-30" == next_air_date(example_list, "1x01")
+    assert "2011-06-30" == next_air_date(@example_list, "1x01")
   end
 
   test "#next_air_date should ignore leading zeros" do
-    assert "2011-06-30" == next_air_date(example_list, "01x01")
+    assert "2011-06-30" == next_air_date(@example_list, "01x01")
     assert "2011-06-30" == next_air_date([["01x01", "2011-06-23"], ["01x02", "2011-06-30"]], "1x01")
   end
 
   test "#next_air_date should return nil if the last index is given" do
-    assert nil == next_air_date(example_list, "01x03")
+    assert nil == next_air_date(@example_list, "01x03")
   end
 
   test "#next_air_date should return nil if there is no next entry" do
-    assert nil == next_air_date(example_list, "20x01")
-  end
-
-  defp example_list do
-    [["1x01", "2011-06-23"], ["1x02", "2011-06-30"], ["1x03", "2011-07-07"]]
+    assert nil == next_air_date(@example_list, "20x01")
   end
 
 end
--- test/whats_next/file_cache_test.exs
+++ test/whats_next/file_cache_test.exs
@@ -1,5 +1,5 @@
 defmodule WhatsNext.FileCacheTest do
-  use ExUnit.Case, async: false
+  use ExUnit.Case
 
   import WhatsNext.FileCache
 
