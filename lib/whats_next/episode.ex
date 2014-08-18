74c5c1b7f92802df675cdf240739e137169fd6c5
Mark Schmidt <mark.schmidt@xing.com>
2014-08-18 22:04:26 +0200

    used recursion to find next air date

--- lib/whats_next/episode.ex
+++ lib/whats_next/episode.ex
@@ -1,18 +1,16 @@
 defmodule WhatsNext.Episode do
 
-  def next_air_date(list, episode) do
-    index = Enum.find_index(list, &same_episode?(hd(&1), episode))
-    #TODO: try to use recursion to get rid of the edge case handling
-    last_index = length(list) - 1
-    case index do
-      nil         -> nil
-      ^last_index -> nil
-      _           -> Enum.at(list, index + 1) |> Enum.at(1)
-    end
-  end
+  def next_air_date(list, episode), do: do_next_air_date(list, episode)
 
-  defp same_episode?(str1, str2) do
-    remove_leading_zero(str1) == remove_leading_zero(str2)
+  defp do_next_air_date([current, next | tail], episode) do
+    if same_episode?(current, episode), do: Enum.at(next, 1),
+    else: do_next_air_date([next|tail], episode)
+  end
+  defp do_next_air_date([_], _), do: nil
+
+
+  defp same_episode?([episode1, _], episode2) do
+    remove_leading_zero(episode1) == remove_leading_zero(episode2)
   end
 
   defp remove_leading_zero(<<head :: integer, tail :: binary>>) when head == 48, do: tail
